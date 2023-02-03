aecbadbcfd457d408b365e4c4db1fc0f1c141b19
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-03 15:17:27 -0500

    Initial Commit

---
+++ Whitelist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Whitelist
+//
+//  Created by Hariz Shirazi on 2023-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whitelist/WhitelistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhitelistApp.swift
+//  Whitelist
+//
+//  Created by Hariz Shirazi on 2023-02-03.
+//
+
+import SwiftUI
+
+@main
+struct WhitelistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
