84d3d1d66da6f31db15b5bc748f611f207368664
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-02-03 15:24:30 -0500

    Add CVE-2022-46689

---
+++ Whitelist/Whitelist-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "vm_unaligned_copy_switch_race.h"
+#import "grant_full_disk_access.h"
+#import "helpers.h"
